8a37acb05e8a6655f29b3ab3a83c850ae9be0b7b
Worlixz <geremydumas@gmail.com>
2021-09-13 21:01:16 +0200

    mise ne place du management de carte

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import arow from '../assets/up-arrow-angle.svg'
 
 function Quiz(props) {
     console.log(props)
     const dataCollection = props.propsHistory.location.state.dataCardsMap
     const dataCards = props.propsHistory.location.state.dataCardsMap.cards
-    console.log(dataCards)
+    console.log("object entries : ",Object.entries(dataCards).map((elementMap) => {
+        console.log(elementMap)
+    }))
+
+    const handlePreviously = (props) => {
+     props.view.location.assign('/carte')
+    }
 
     return (
-        <div>
-            Je suis dans Quiz et {dataCollection.nameCollection}
+        <div className="containerQuiz">
+            <button onClick={handlePreviously} id="btnArowPreviously"><img id="arowPreviously" src={arow}/></button>
+            <h2>Collection : {dataCollection.nameCollection}</h2> 
+            <button className="btnGo">C'est parti !</button>
         </div>
     )
 }
