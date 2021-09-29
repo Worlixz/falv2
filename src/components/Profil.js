7dc89e83a566a8fc99e4532b0b6662219b43b16d
Worlixz <geremydumas@gmail.com>
2021-09-29 20:57:26 +0200

    avancement composant profil

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -2,41 +2,79 @@ import React, {useState, useContext, useEffect} from 'react'
 import Blob from '../assets/Blob.png'
 import { FirebaseContext } from './Firebase/index'
 import Loader from './Loader'
+import iconProfil from '../assets/sidebar/icon_profil_gris.png'
+import pencil from '../assets/CardsManagement/pencil.svg'
+import crossRed from '../assets/CardsManagement/crossRedClose.svg'
 
 function Profil(props) {
 
     const firebase = useContext(FirebaseContext)
     
     const [userSession, setUserSession] = useState(null)
-    const [dataProfil, setDataProfil] = useState(props.dataUser)
+    const [dataProfil, setDataProfil] = useState({
+        userName: '',
+        email: '',
+        nomPrenom: '',
+        etude: '',
+        profilPicture: ''
+    })
+
+    const [modeEdition, setModeEdition] = useState(false)
 
     useEffect(() => {
         let listener = firebase.auth.onAuthStateChanged(user => {
             user ? setUserSession(user) : props.history.push('/')
         })
+
+        if(userSession !== null) {
+            firebase.userData(userSession.uid)
+            .get()
+            .then((doc) => {
+                if(doc.exists){
+                    console.log(doc.data())
+                    const myData = doc.data()
+                    setDataProfil({
+                        userName: myData.userName ? myData.userName : "A renseigner",
+                        email: myData.email ? myData.email : "A renseigner",
+                        nomPrenom: myData.nomPrenom ? myData.nomPrenom : "A renseigner",
+                        etude: myData.etude ? myData.etude : "A renseigner",
+                        profilPicture: myData.profilPicture ? myData.profilPicture : iconProfil
+                    })
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
         return () => {
             listener()
+        };
         }
     }, [userSession])
 
+    const handleEditionMode = () => {
+        setModeEdition(!modeEdition)
+    }
 
-    console.log("Props Profil : ", dataProfil)
+    const handleChange = (e) => {
+        setDataProfil({...dataProfil, [e.target.id]: e.target.value})
+    }
 
     const selector = (
-        <select>
+        <select id="etude">
             <optgroup label="-- STAPS --">
-                <option>Staps - Licence 1</option>
-                <option>Staps - Licence 2</option>
-                <option>Staps - Licence 3</option>
-                <option>Staps - Master 1</option>
-                <option>Staps - Master 2</option>
+                <option value="Staps - Licence 1">Staps - Licence 1</option>
+                <option value="Staps - Licence 2">Staps - Licence 2</option>
+                <option value="Staps - Licence 3">Staps - Licence 3</option>
+                <option value="Staps - Master 1">Staps - Master 1</option>
+                <option value="Staps - Master 2">Staps - Master 2</option>
             </optgroup>
             <optgroup label="-- OSTEOPHATIE --">
-                <option>Ostéophatie - Licence 1</option>
-                <option>Ostéophatie - Licence 2</option>
-                <option>Ostéophatie - Licence 3</option>
-                <option>Ostéophatie - Master 1</option>
-                <option>Ostéophatie - Master 2</option>
+                <option value="Ostéophatie - Licence 1">Ostéophatie - Licence 1</option>
+                <option value="Ostéophatie - Licence 1">Ostéophatie - Licence 2</option>
+                <option value="Ostéophatie - Licence 1">Ostéophatie - Licence 3</option>
+                <option value="Ostéophatie - Master 1">Ostéophatie - Master 1</option>
+                <option value="Ostéophatie - Master 2">Ostéophatie - Master 2</option>
             </optgroup>
         </select>
     )
@@ -48,22 +86,26 @@ function Profil(props) {
             <div className="blobProfil">
                 <img src={Blob} />
                 <div className="containerInfoImg">
-                    <h3>Nom - Prénom</h3>
-                    <p>DOE John</p>
+                    {modeEdition ? (<p id="paraModifIMG">Mettre une image d'une grandeur de 150*150px <br />Pour redimentionnser votre image :<a target="_blank" href="https://convert-my-image.com/ImageConverter_Fr"> C'est par ici !</a></p>) : (null)}
+                    <img src={dataProfil.profilPicture} />
+                    {modeEdition ? (<img onClick={handleEditionMode} id="modificationPencil" src={crossRed} />) : (<img onClick={handleEditionMode} id="modificationPencil" src={pencil} />)}
+                    {modeEdition ? (
+                        <input type="file" id="profilPicture" name="profile_pic"
+                            accept=".jpg, .jpeg, .png"></input>) : (null)}
                 </div>
             </div>
             <div className="displayInfo">
-                {/* <div className="containerInfo">
-                    <h3>Nom - Prénom</h3>
-                    <p>DOE John</p>
-                </div> */}
                 <div className="containerInfo">
-                    <h3>Pseudo</h3>
-                    <p>{/* {dataProfil.userName} */}</p>
+                    <h3>Nom - Prénom</h3>
+                    {modeEdition ? (<input onChange={handleChange} id="nomPrenom" value={dataProfil.nomPrenom} />) : (<p>{dataProfil.nomPrenom}</p>)}
                 </div>
                 <div className="containerInfo">
+                    <h3>Pseudo</h3>
+                    {modeEdition ? (<input onChange={handleChange} id="userName" value={dataProfil.userName} />) : (<p>{dataProfil.userName}</p>)}
+                </div> 
+                <div className="containerInfo">
                     <h3>Email</h3>
-                    <p>{/* {dataProfil.email} */}</p>
+                    <p>{dataProfil.email}</p>
                 </div>
                 <div className="containerInfo">
                     <h3>Mot de passe</h3>
@@ -71,8 +113,30 @@ function Profil(props) {
                 </div>
                 <div className="containerInfo">
                     <h3>Etude</h3>
-                    <p>Ostéophatie - Lience 2</p>
+                    {modeEdition ? (
+                        <select onChange={handleChange} value={dataProfil.etude} id="etude">
+                            <optgroup label="-- STAPS --">
+                                <option value="Staps - Licence 1">Staps - Licence 1</option>
+                                <option value="Staps - Licence 2">Staps - Licence 2</option>
+                                <option value="Staps - Licence 3">Staps - Licence 3</option>
+                                <option value="Staps - Master 1">Staps - Master 1</option>
+                                <option value="Staps - Master 2">Staps - Master 2</option>
+                            </optgroup>
+                            <optgroup label="-- OSTEOPHATIE --">
+                                <option value="Ostéophatie - Licence 1">Ostéophatie - Licence 1</option>
+                                <option value="Ostéophatie - Licence 1">Ostéophatie - Licence 2</option>
+                                <option value="Ostéophatie - Licence 1">Ostéophatie - Licence 3</option>
+                                <option value="Ostéophatie - Master 1">Ostéophatie - Master 1</option>
+                                <option value="Ostéophatie - Master 2">Ostéophatie - Master 2</option>
+                            </optgroup>
+                    </select>
+                    ) : (<p>{dataProfil.etude}</p>)}
+                    
                 </div>
+                {modeEdition ? (<div className="containerInfo">
+                    <button id="sauvegarde">Sauvegarder</button>
+                </div>) : null}
+                
             </div>
         </div>
     )
